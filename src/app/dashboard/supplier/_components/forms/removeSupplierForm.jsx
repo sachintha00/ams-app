8a90bd3d3dcbc09f9c2a1fb91bfa8d39856de729
sachintha00 <sachintha0603@gmail.com>
2024-05-16 17:07:59 +0530

    15 Feat: Supplier Registration

---
+++ src/app/dashboard/supplier/_components/forms/removeSupplierForm.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { useDispatch, useSelector } from "react-redux";
+import * as Yup from "yup";
+import {
+    useRemoveSupplierMutation,
+} from "@/app/_lib/redux/features/supplier/supplier_api";
+import { handleClosePopupModel } from "@/app/_lib/redux/features/popupModel/popupModelSlice";
+
+const RemoveWorkflowSchema = Yup.object().shape({
+    name: Yup.string()
+        .required("Please enter the supplier name")
+        .test("match-name", "Value does not match", function (value) {
+            return value === this.resolve(Yup.ref("deleterowname"));
+        }),
+});
+
+function RemoveSupplierForm() {
+    const dispatch = useDispatch();
+    const [removeSupplier] =
+        useRemoveSupplierMutation();
+    const { id, value } = useSelector((state) => state.popupModel);
+
+    return (
+        <div>
+            <Formik
+                initialValues={{
+                    name: "",
+                    deleterowname: value,
+                }}
+                validationSchema={RemoveWorkflowSchema}
+                onSubmit={async (values, { resetForm }) => {
+
+                    try {
+                        await removeSupplier(id).unwrap();
+                        dispatch(handleClosePopupModel());
+                        console.log("supllier  deleted successfully");
+                    } catch (error) {
+                        console.error("Failed to delete the supplier", error);
+                    }
+                }}
+            >
+                {({ touched, errors }) => (
+                    <Form className="px-2 pt-2">
+                        <h3 className="text-lg font-semibold text-gray-900 dark:text-white w-[80%] mb-2 mt-2">
+                            If you want to delete {value}, please enter "{value}" below
+                        </h3>
+                        <div className="grid grid-cols-2 gap-4 mb-4">
+                            <div className="col-span-2">
+                                <Field
+                                    type="text"
+                                    name="name"
+                                    id="name"
+                                    className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-[#3c4042] dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 ${touched.name && errors.name ? "border-red-500" : ""
+                                        }`}
+                                    required
+                                />
+                                <ErrorMessage
+                                    name="name"
+                                    component="div"
+                                    className="text-sm text-red-500"
+                                />
+                            </div>
+                        </div>
+                        <button
+                            type="submit"
+                            className="text-white inline-flex items-center bg-[#D32D41] hover:bg-[#D32D41] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-[#D32D41] dark:hover:bg-[#D32D41] dark:focus:ring-blue-800"
+                        >
+                            Delete
+                        </button>
+                    </Form>
+                )}
+            </Formik>
+        </div>
+    );
+}
+
+export default RemoveSupplierForm;
